9078d2d6838fd1ffa6ab2892eafa2ef6b8d68743
Saad05-dev <bougrasaad@gmail.com>
2025-03-26 21:55:29 +0000

    Game Functions

--- Game.cpp
+++ Game.cpp
@@ -8,20 +8,82 @@ void Game::initVariables()
     this->window = nullptr;
 
     //Game logic
+    this->Fruitspawn = 1;
+    this->endGame = false;
+    this->FruitspawnTimerMax = 10.f;
+    this->FruitspawnTimer = this->FruitspawnTimerMax;
 }
 //Set Game window
 void Game::initWindow()
 {
     this->videoMode.height = 600;
     this->videoMode.width = 800;
-    this->window = new sf::RenderWindow(this->videoMode,"Snake Game",sf::Style::Titlebar | sf::Style::Close );
+    this->window = new sf::RenderWindow(this->videoMode,"Snake Game"
+        ,sf::Style::Titlebar | sf::Style::Close );
     this->window->setFramerateLimit(60);
+}
+//initialize Fruit
+void Game::initFruits()
+{
+    this->Fruit.setPosition(10.f,10.f);
+    this->Fruit.setSize(sf::Vector2f(50.f,50.f));
+    this->Fruit.setScale(sf::Vector2f(0.5f,0.5f));
+    this->Fruit.setFillColor(sf::Color::Red);
+}
+//Ends the game
+const bool Game::getEndGame() const
+{
+    return this->endGame;
+}
+//Spawn fruit
+void Game::spawnFruit()
+{
+    /*
+        return void
+        Spawns Fruit 
+        -Sets a randow position
+    */
+   this->Fruit.setPosition(
+    static_cast<float>(rand() % static_cast<int>(this->window->getSize().x 
+        - this->Fruit.getSize().x)),
+    static_cast<float>(rand() % static_cast<int>(this->window->getSize().y
+        - this->Fruit.getSize().y))
+   );
+   // Spawn Fruit
+   this->Fruits.push_back(this->Fruit);
+}
+//Updates position of Fruit
+void Game::updateFruits()
+{
+    /*
+        return void
+        updates the Fruit spawn 
+    */
+   //update spawn timer
+   if(this->Fruits.size() < 1)
+   {
+    if(this->FruitspawnTimer >= this->FruitspawnTimerMax)
+    {
+        this->spawnFruit();
+        this->FruitspawnTimer = 0.f;
+    }
+    else
+    {
+        this->FruitspawnTimer += 1.f;
+    }
+   }
+}
+void Game::renderFruits(sf::RenderTarget& target)
+{
+    //render fruit
+    target.draw(this->Fruit);
 }
 // Constructors / Deconstructors
 Game::Game()
 {
     this->initVariables();
     this->initWindow();
+    this->initFruits();
 }
 Game::~Game()
 {
@@ -60,6 +122,10 @@ void Game::update()
         update fruit
     */
     this->pollEvents();
+    if(this->endGame == false)
+    {
+        this->updateFruits();
+    }
 }
 void Game::render()
 {
@@ -71,6 +137,9 @@ void Game::render()
         render the game objects
     */
    this->window->clear(sf::Color(141,161,89));
+
+   //Draw game objects
+   this->renderFruits(*this->window);
 
    this->window->display();
 }
