921ad7ac884e1575d988e28fca600d831c4eda82
Saad05-dev <bougrasaad@gmail.com>
2025-03-27 23:46:03 +0000

    Snake class

---
+++ Snake.h
@@ -0,0 +1,36 @@
+#pragma once 
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+// class Snake
+class Snake
+{
+private:
+    
+    sf::RectangleShape snake;
+
+    //Snale Movement speed
+    float movementSpeed;
+
+    void initVariables();
+    void initShape();
+
+public:
+    Snake(float x = 0.f,float y = 0.f);
+    ~Snake();
+
+    void updateInput();
+    void update();
+    void render(sf::RenderTarget* target);
+};
+
+
