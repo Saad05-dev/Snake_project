ee770616155b0487eca75d7f7a82e1d95ef08d84
Saad05-dev <bougrasaad@gmail.com>
2025-03-27 23:46:18 +0000

    Snake functions

---
+++ Snake.cpp
@@ -0,0 +1,51 @@
+#include "Snake.h"
+
+//Initialize variables
+void Snake::initVariables()
+{
+    this->movementSpeed = 10.f;
+}
+//Initialize Snake's shape
+void Snake::initShape()
+{
+    this->snake.setFillColor(sf::Color::Blue);
+    this->snake.setSize(sf::Vector2f(50.f,50.f));
+}
+
+Snake::Snake(float x,float y)
+{
+    this->snake.setPosition(x,y);
+
+    this->initVariables();
+    this->initShape();
+}
+
+Snake::~Snake()
+{
+}
+//Snake input control
+void Snake::updateInput()
+{
+    //Keyboard input
+    //left
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+    {
+        this->snake.move(-this->movementSpeed,0.f);
+    }
+    //right
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        this->snake.move(this->movementSpeed,0.f);
+    }
+    //up
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+    {
+        this->snake.move(0.f,-this->movementSpeed);
+    }
+    //down
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        this->snake.move(0.f,this->movementSpeed);
+    }
+    //window bound
+}
