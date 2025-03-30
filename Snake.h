3302b99da89dba9fe0c6fbf6fc97feff72a42a4c
Saad05-dev <bougrasaad@gmail.com>
2025-03-30 02:51:42 +0000

    Snake class

--- Snake.h
+++ Snake.h
@@ -1,15 +1,19 @@
 #pragma once 
 #include "Fruit.h"
 
+//Enum represents snake movement
+enum class Direction {UP, DOWN, LEFT, RIGHT};
+
 // class Snake
 class Snake
 {
 private:
     
-    sf::RectangleShape snake;
+    std::vector<sf::RectangleShape> snake; //holds the snake's whole segment
 
     //Snake Movement speed
     float movementSpeed;
+    Direction direction;
 
     void initVariables();
     void initShape();
@@ -18,12 +22,18 @@ public:
     Snake(float x = 0.f,float y = 0.f);
     ~Snake();
 
-    const sf::RectangleShape& snakeShape() const;
+    //accessors
+    const std::vector<sf::RectangleShape>& getBody() const;
 
+    //functions
     void updateInput();
     void SnakePos(float x = 0.f,float y = 0.f);
+    void snakeShape();
     void updateWindowBoundsCoollision(const sf::RenderTarget* target);
     void update(const sf::RenderTarget* target);
+    void grow();
+    void move();
+    bool snakeCollision();
     void render(sf::RenderTarget* target);
 };
 
